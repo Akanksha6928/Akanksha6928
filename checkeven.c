5aaefce69d42b6de6d855738f685233920902c2b
Akanksha6928 <payalthakur6928@gmail.com>
2024-09-27 16:28:33 +0530

    Add files via upload

---
+++ checkeven.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+ int main()
+{
+   int num;
+   printf("enter num ");
+   scanf("%d" ,&num);
+   if(num%2==0)
+   printf("even");
+   else
+   printf("odd");
+    return 0;
+    
+}
+
